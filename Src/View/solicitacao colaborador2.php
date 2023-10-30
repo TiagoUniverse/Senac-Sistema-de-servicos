ca0dabcb2e0b7dcab074eb7b1228f095be6ebb72
TiagoUniverse <tiagocesar68@gmail.com>
2023-10-30 14:03:35 -0300

    Validação do cpf

--- Src/View/solicitacao colaborador2.php
+++ Src/View/solicitacao colaborador2.php
@@ -11,11 +11,11 @@
  * ║  │ a menos que seja obtida permissão prévia por escrito do SENAC PERNAMBUCO.                                   │  ║
  * ║  └─────────────────────────────────────────────────────────────────────────────────────────────────────────────┘  ║
  * ║  ┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────┐  ║
- * ║  │ @description: Tela inicial para solicitar o aceite do colaborador em um serviço                             │  ║
- * ║  │ @class: solicitacao colaborador                                                                             │  ║
+ * ║  │ @description: Resultado da solicitação de colaborador de um serviço                                         │  ║
+ * ║  │ @class: solicitacao colaborador2                                                                            │  ║
  * ║  │ @dir: View                                                                                                  │  ║
  * ║  │ @author: Tiago César da Silva Lopes                                                                         │  ║
- * ║  │ @date: 27/10/23                                                                                             │  ║
+ * ║  │ @date: 30/10/23                                                                                             │  ║
  * ║  └─────────────────────────────────────────────────────────────────────────────────────────────────────────────┘  ║
  * ║═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════║
  * ║                                                     UPGRADES                                                      ║
@@ -29,27 +29,9 @@
 
 require_once "conexao.php";
 
-require_once "conexao_TermoAceite.php";
 
 // Variáveis
-if (isset($_POST['nomeServico'])) {
-  $_SESSION['nomeServico'] = trim($_POST['nomeServico']);
-}
 
-
-if (isset($_POST['idServico'])) {
-  $_SESSION['idServico'] = trim($_POST['idServico']);
-}
-
-
-
-/*
-* ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
-* │  ValidaCPF'S SECTION                                                                                          │
-* └───────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
-*/
-
-require_once "./Recursos/CpfValidacao.php";
 
 /*
 * ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
@@ -64,18 +46,6 @@ use model\Servicos_repositorio;
 
 $Servico_repositorio = new Servicos_repositorio();
 
-/*
-* ┌───────────────────────────────────────────────────────────────────────────────────────────────────────────────┐
-* │  TOTVS'S SECTION                                                                                              │
-* └───────────────────────────────────────────────────────────────────────────────────────────────────────────────┘
-*/
-
-require_once "../model/TOTVS_repositorio.php";
-
-
-use model\TOTVS_repositorio;
-
-$TOTVS_repositorio = new TOTVS_repositorio();
 
 
 
@@ -115,121 +85,7 @@ $TOTVS_repositorio = new TOTVS_repositorio();
         <button class="botao-voltar" type="submit">Voltar</button>
       </form>
 
-      <h1 class="header center black-text"> <?php echo  $_SESSION['nomeServico'] . ": Solicitação de colaborador" ?> </h1>
-
-      <?php
-      if (!isset($_POST['status_cadastro'])) {
-      ?>
-        <div class="row center">
-          <h5 class="header col s12 light">Comece o cadastro informando o CPF do colaborador. </h5>
-        </div>
-
-        <!-- <h3 class="header col s12 light"> 1. Informações gerais </h3> -->
-        <form action="solicitacao colaborador.php" method="post" enctype="multipart/form-data">
-          <div class="row center">
-            <div class="input-field col s12">
-              <input type="hidden" name="status_cadastro" value="SOLICITANDO UM NOVO COLABORADOR">
-
-              <div class="row">
-                <div class="row">
-                  <div class="input-field col s12">
-                    <input value="" name="cpf" id="cpf" type="number" onKeyDown="if(this.value.length==13 && event.keyCode!=8) return false;" class="validate" required>
-                    <label for="cpf">CPF:</label>
-                  </div>
-                </div>
-
-              </div>
-
-            </div>
-          </div>
-
-
-          <div class="row center">
-            <button type="submit" id="download-button" class="btn-large waves-effect waves-light orange">Continuar</button>
-          </div>
-
-        </form>
-
-        <?php
-      } else {
-        $validacaoCPF = validaCPF($_POST['cpf']);
-
-
-        if ($validacaoCPF) {
-
-          $TOTVS = $TOTVS_repositorio->consultar_cpf($_POST['cpf'], $pdo_TermoAceite);
-
-          if ($TOTVS == false) {
-        ?>
-            <br>
-            <div class="div-error">
-              <h3 class='header center col s12 light'> Colaborador não registrado no TOTVS. Por favor, tente com outro CPF. </h3>
-            </div>
-          <?php
-          } else {
-          ?>
-
-            <br>
-            <h3 class="header col s12 light"> CPF validado. Prossiga com o a solicitação </h3>
-            <form action="solicitacao colaborador2.php" method="post" enctype="multipart/form-data">
-              <div class="row center">
-                <div class="input-field col s12">
-                  <input type="hidden" name="cpf" value="<?php echo $_POST['cpf']; ?> ">
-
-                  <div class="row">
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input value="" name="nomeColaborador" id="nomeColaborador" type="text" class="validate" required>
-                        <label for="nomeColaborador">Nome do colaborador:</label>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input value=" <?php echo $_POST['cpf']; ?> " name="cpf" id="cpf" type="text" class="validate" disabled>
-                        <label for="cpf">CPF:</label>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input value="" name="email_pessoal" id="email_pessoal" type="email" class="validate" required>
-                        <label for="email_pessoal">E-mail pessoal do colaborador:</label>
-                      </div>
-                    </div>
-
-                    <div class="row">
-                      <div class="input-field col s12">
-                        <input value="" name="telefone" id="telefone" oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" type="number" maxlength="15" class="validate" required>
-                        <label for="telefone">Telefone:</label>
-                      </div>
-                    </div>
-                  </div>
-
-                </div>
-              </div>
-
-
-              <div class="row center">
-                <button type="submit" id="download-button" class="btn-large waves-effect waves-light orange">Cadastrar</button>
-              </div>
-
-            </form>
-
-
-          <?php
-          }
-        } else {
-          ?>
-          <br>
-          <div class="div-error">
-            <h3 class='header center col s12 light'> CPF inválido. Por favor, tente novamente. </h3>
-          </div>
-      <?php
-        }
-      }
-
-      ?>
+      
 
 
 
